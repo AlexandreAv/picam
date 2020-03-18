886dbd89eed02accaeb9e2fedbbb5772e44843d2
Alexandre <alexandre.rangom@mail.com>
2020-03-18 18:18:19 +0100

    Premier commit

---
+++ detectors/SSDDetectors.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+from math import ceil
+import cv2
+
+
+class SSDDetectors:
+	"""
+		Classe de détection charger d'utiliser les modèles tensorflow.
+		Cette classe va permettre de rendre l'utilisation des modèles plus facile
+
+		:param
+		@path_model: path_model est une chaine de caractère chargé d'indiquer l'emplacement du modèle tensorflow sauvegarder dans un format SavedModel
+		@path_label: path_label est une chaine de caractère d'indiquer l'emplacement d'un fichier textes indiquants les catégories possibles
+	"""
+
+	def __init__(self, path_model, path_label):
+		self.model = tf.saved_model.load(path_model + '/saved_model')
+		self.model = self.model.signatures['serving_default']
+		self.list_label = self.read_labels(path_label)
+
+	@staticmethod
+	def read_labels(path_label):  # on lit le txt puis on renvoit les valeurs grâce à un split
+		data = []
+		with open(path_label) as file:
+			for line in file.readlines():
+				data.append(line.replace("\n", "").split("\t")) # on supprime la première ligne qui est une légende et non des valeurs utiles
+
+		del data[0]
+		return data  # on retourne les valeurs du txt
+
+	def get_id_category(self, id):  #  on récupère la catégorie d'un objet grâce à l'identifiant de celui-ci
+		for element in self.list_label:
+			# pdb.set_trace()
+			if int(element[0]) == id:
+				return element[1]
+
+		print("l'id {} est inconnu".format(id))
+		return ""
+
+	def run_model_and_clean_output(self, batch_img):
+		predictions = self.model(batch_img)
+
+		num_detections = predictions.pop('num_detections').numpy().astype(dtype='int')
+		len_predictions = len(num_detections)
+		predictions = [{key: value.numpy()[i][0: num_detections[i]] for key, value in predictions.items()}
+					   for i in range(len_predictions)]
+
+		return predictions
+
+	def drawn_bounding_boxes(self, batch_img, batch_preds, color=(0, 255, 0)):
+		batch_img = batch_img.numpy()
+
+		for i_1 in range(len(batch_img)):
+			img = batch_img[i_1]
+			img_w, img_h, _ = img.shape
+			predictions = batch_preds[i_1]
+			score = predictions[0]
+			class_id = predictions[1]
+			bbox = predictions[2]
+
+			for i_2 in range(len(score)):  # ymin, xmin, ymax, xmax = box
+				coord_min = (int(bbox[i_2][3] * img_w), int(bbox[i_2][2] * img_h))
+				coord_max = (int(bbox[i_2][1] * img_w), int(bbox[i_2][0] * img_h))
+				org = (coord_max[0] + 10, coord_max[1] + 20)
+				batch_img[i_1] = cv2.rectangle(img, coord_max, coord_min, color, 2)
+				text = "{}% {}".format(ceil(score[i_2] * 100), self.get_id_category(int(class_id[i_2])))
+				batch_img[i_1] = cv2.putText(img, text, org, cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+											 color)  # TODO présenter correctement les params
+
+		return batch_img
+
+	def run_detection(self, batch_img):
+		prediction = self.run_model_and_clean_output(batch_img)
+		len_results = len(prediction)
+
+		batch_preds = [list(prediction[i].values()) for i in range(len_results)]
+
+		return self.drawn_bounding_boxes(batch_img, batch_preds)
---
+++ main.py
@@ -0,0 +1,83 @@
+import sys
+import tensorflow as tf
+import numpy as np
+import cv2
+from time import time
+from detectors.SSDDetectors import SSDDetectors
+import pdb
+
+# TODO optimiser tflite
+
+
+def mean(x):
+	somme = 0
+
+	for n in x:
+		somme += n
+
+	return round(somme / len(x))
+
+
+def main(num_cam, path_to_model, path_label, input_size, output_size, display, time_of_cap):
+	detector = SSDDetectors(path_to_model, path_label)
+	cap = cv2.VideoCapture(num_cam)
+	video = cv2.VideoWriter("video.mkv", cv2.VideoWriter_fourcc(*'MJPG'), 25, output_size)
+	t0 = time()
+	tab_fps = []
+	t_fps = time()
+	fps = 0
+	num_frame = 0
+
+	while True:
+		ret, frame = cap.read()
+
+		# Our operations on the frame come here
+		frame = cv2.resize(frame, input_size)
+		frame = tf.convert_to_tensor(np.expand_dims(frame, axis=0), dtype=tf.uint8)
+		output_frame = detector.run_detection(frame)[0]
+		output_frame = cv2.resize(output_frame, output_size)
+		output_frame = cv2.putText(output_frame, str(fps), (15, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+		video.write(output_frame)
+		# pdb.set_trace()
+
+		# Get fps
+		num_frame += 1
+		if num_frame == 4:
+			fps = round(num_frame / abs(t_fps - time()), 1)
+			num_frame = 0
+			t_fps = time()
+			tab_fps.append(fps)
+
+		# Display the resulting frame
+		if display:
+			cv2.imshow('frame', output_frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+
+		if abs(t0 - time()) > time_of_cap:
+			break
+
+	if display:
+		cv2.destroyAllWindows()
+	cap.release()
+
+	video.set(cv2.VIDEOWRITER_PROP_FRAMEBYTES, mean(tab_fps))
+	video.release()
+
+
+if __name__ == '__main__':  # temps en seconde
+	with open('detector-config.txt') as file:
+		data = []
+		for line in file.readlines():
+			data.append(line.replace("\n", "").split("#")[0].split('='))
+
+		capture = int(data[0][1])
+		path_model = data[1][1]
+		path_label = data[2][1]
+		inputs_size = (int(data[3][1]), int(data[3][1]))
+		outputs_size = (int(data[4][1]), int(data[4][1]))
+		show_capture = data[5][1]
+		time_cap = int(sys.argv[1]) if len(sys.argv) == 2 else int(data[6][1])
+
+		main(capture, path_model, path_label, inputs_size, outputs_size, show_capture, time_cap)
+
