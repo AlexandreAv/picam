e3887f4121358dcac2fbae3fd703993df42e8b31
Alexandre Rangom <alexandre.rangom@gmail.com>
2020-03-29 18:34:25 +0200

    <feat>(<main.py, detector-config.txt>): <Ajustement du code pour portage sur docker> <Ajout de ligne de configuration tensorflow nécessaire au maintien de bonne performance avec docker puis actualisation des chemins des fichiers externe pour docker>

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import pdb
 
 # TODO optimiser tflite
 
-
 def mean(x):
 	somme = 0
 
@@ -18,55 +17,68 @@ def mean(x):
 	return round(somme / len(x))
 
 
+class VideoCapture(object):
+	def __init__(self, num_cam):
+		self.num_cam = num_cam
+	def __enter__(self):
+		self.cap = cv2.VideoCapture(self.num_cam)
+		return self.cap
+	def __exit__(self, type, value, traceback):
+		self.cap.release()
+		print('la caméra a été libéré')
+
+
+
 def main(num_cam, path_to_model, path_label, input_size, output_size, display, time_of_cap):
 	detector = SSDDetectors(path_to_model, path_label)
-	cap = cv2.VideoCapture(num_cam)
-	video = cv2.VideoWriter("video.mkv", cv2.VideoWriter_fourcc(*'MJPG'), 25, output_size)
+	video = cv2.VideoWriter("picam/video.mkv", cv2.VideoWriter_fourcc(*'MJPG'), 25, output_size)
 	t0 = time()
 	tab_fps = []
 	t_fps = time()
 	fps = 0
 	num_frame = 0
 
-	while True:
-		ret, frame = cap.read()
 
-		# Our operations on the frame come here
-		frame = cv2.resize(frame, input_size)
-		frame = tf.convert_to_tensor(np.expand_dims(frame, axis=0), dtype=tf.uint8)
-		output_frame = detector.run_detection(frame)[0]
-		output_frame = cv2.resize(output_frame, output_size)
-		output_frame = cv2.putText(output_frame, str(fps), (15, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-		video.write(output_frame)
-		# pdb.set_trace()
+	with VideoCapture(num_cam) as cap:
+		while True:
+			ret, frame = cap.read()
 
-		# Get fps
-		num_frame += 1
-		if num_frame == 4:
-			fps = round(num_frame / abs(t_fps - time()), 1)
-			num_frame = 0
-			t_fps = time()
-			tab_fps.append(fps)
+			# Our operations on the frame come here
+			frame = cv2.resize(frame, input_size)
+			frame = tf.convert_to_tensor(np.expand_dims(frame, axis=0), dtype=tf.uint8)
+			output_frame = detector.run_detection(frame)[0]
+			output_frame = cv2.resize(output_frame, output_size)
+			output_frame = cv2.putText(output_frame, str(fps), (15, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+			video.write(output_frame)
+			# pdb.set_trace()
 
-		# Display the resulting frame
-		if display:
-			cv2.imshow('frame', output_frame)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
+			# Get fps
+			num_frame += 1
+			if num_frame == 4:
+				fps = round(num_frame / abs(t_fps - time()), 1)
+				num_frame = 0
+				t_fps = time()
+				tab_fps.append(fps)
+
+			# Display the resulting frame
+			if display:
+				cv2.imshow('frame', output_frame)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+
+			if abs(t0 - time()) > time_of_cap:
 				break
 
-		if abs(t0 - time()) > time_of_cap:
-			break
 
-	if display:
-		cv2.destroyAllWindows()
-	cap.release()
+		if display:
+			cv2.destroyAllWindows()
 
 	video.set(cv2.VIDEOWRITER_PROP_FRAMEBYTES, mean(tab_fps))
 	video.release()
 
 
 if __name__ == '__main__':  # temps en seconde
-	with open('detector-config.txt') as file:
+	with open('picam/detector-config.txt') as file:
 		data = []
 		for line in file.readlines():
 			data.append(line.replace("\n", "").split("#")[0].split('='))
