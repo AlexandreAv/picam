7de22fd4fbefde4d354ffa72f46c5df6fe7ac388
Alexandre Rangom <alexandre.rangom@gmail.com>
2020-03-30 19:31:41 +0200

    <feat>(depencies/VideoCapture.py, main.py): <Mise en place d'une classe VideoCapture pour augmenter la lisibilite du code> <Class VideoCapture facilite l'acces à la webcam et gere toute seule les fps, consequence, ameliorant de la lisibilite de la boucle principale>

---
+++ depencies/VideoCapture.py
@@ -0,0 +1,55 @@
+import cv2
+from time import time
+
+class VideoCapture(object):
+	def __init__(self, num_cam, time_of_cap, display):
+		self.cap = None
+		self.num_cam = num_cam
+		self.time_of_cap = time_of_cap
+		self.display = display
+		self.t0 = 0
+		self.t_fps = 0
+		self.fps = 0
+		self.num_frame = 0
+		self.tab_fps = []
+
+	def read(self):
+		return self.cap.read()
+
+	def is_opened(self):
+		return self.cap.isOpened()
+
+	def time_is_passed(self):
+		if not self.time_of_cap:
+			return False
+
+		return abs(self.t0 - time() > self.time_of_cap)
+
+	def get_fps(self):
+		self.num_frame += 1
+		if self.num_frame == 4:
+			self.fps = round(self.num_frame / abs(self.t_fps - time()), 1)
+			self.num_frame = 0
+			self.t_fps = time()
+			self.tab_fps.append(self.fps)
+
+		return self.fps
+
+	def get_mean_fps(self):
+		somme = 0
+
+		for fps in self.tab_fps:
+			somme += fps
+
+		return round(somme / len(self.tab_fps))
+
+	def __enter__(self):
+		self.cap = cv2.VideoCapture(self.num_cam)
+		self.t0 = time()
+		return self
+
+	def __exit__(self, type, value, traceback):
+		self.cap.release()
+		if self.display:
+			cv2.destroyAllWindows()
+		print('la caméra a été libéré')
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import numpy as np
 import cv2
 from time import time
 from detectors.SSDDetectors import SSDDetectors
+from depencies.VideoCapture import VideoCapture
 import pdb
+
 
 # TODO optimiser tflite
 
@@ -17,63 +19,41 @@ def mean(x):
 	return round(somme / len(x))
 
 
-class VideoCapture(object):
-	def __init__(self, num_cam):
-		self.num_cam = num_cam
-	def __enter__(self):
-		self.cap = cv2.VideoCapture(self.num_cam)
-		return self.cap
-	def __exit__(self, type, value, traceback):
-		self.cap.release()
-		print('la caméra a été libéré')
-
-
-
 def main(num_cam, path_to_model, path_label, input_size, output_size, display, time_of_cap):
 	detector = SSDDetectors(path_to_model, path_label)
 	video = cv2.VideoWriter("picam/video.mkv", cv2.VideoWriter_fourcc(*'MJPG'), 25, output_size)
-	t0 = time()
-	tab_fps = []
-	t_fps = time()
-	fps = 0
-	num_frame = 0
+
+	with VideoCapture(num_cam, time_of_cap, display) as cap:
+		if cap.is_opened():
+			while True:
+				ret, frame = cap.read()
+
+				# Our operations the frame come here
+				frame = cv2.resize(frame, input_size)
 
 
-	with VideoCapture(num_cam) as cap:
-		while True:
-			ret, frame = cap.read()
+				if is_there_motion:
+					frame = tf.convert_to_tensor(np.expand_dims(frame, axis=0), dtype=tf.uint8)
+					output_frame = detector.run_detection(frame)[0]
 
-			# Our operations on the frame come here
-			frame = cv2.resize(frame, input_size)
-			frame = tf.convert_to_tensor(np.expand_dims(frame, axis=0), dtype=tf.uint8)
-			output_frame = detector.run_detection(frame)[0]
-			output_frame = cv2.resize(output_frame, output_size)
-			output_frame = cv2.putText(output_frame, str(fps), (15, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-			video.write(output_frame)
-			# pdb.set_trace()
+				output_frame = cv2.resize(output_frame, output_size)
+				output_frame = cv2.putText(output_frame, cap.get_fps(), (15, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+				video.write(output_frame)
+				# pdb.set_trace()
 
-			# Get fps
-			num_frame += 1
-			if num_frame == 4:
-				fps = round(num_frame / abs(t_fps - time()), 1)
-				num_frame = 0
-				t_fps = time()
-				tab_fps.append(fps)
 
-			# Display the resulting frame
-			if display:
-				cv2.imshow('frame', output_frame)
-				if cv2.waitKey(1) & 0xFF == ord('q'):
+				# Display the resulting frame
+				if display:
+					cv2.imshow('frame', output_frame)
+					if cv2.waitKey(30) & 0xFF == ord('q'):
+						break
+
+				if cap.time_is_passed():
 					break
+		else:
+			print('La caméra na pas été ouverte')
 
-			if abs(t0 - time()) > time_of_cap:
-				break
-
-
-		if display:
-			cv2.destroyAllWindows()
-
-	video.set(cv2.VIDEOWRITER_PROP_FRAMEBYTES, mean(tab_fps))
+	video.set(cv2.VIDEOWRITER_PROP_FRAMEBYTES, cap.get_mean_fps())
 	video.release()
 
 
@@ -88,8 +68,7 @@ if __name__ == '__main__':  # temps en seconde
 		path_label = data[2][1]
 		inputs_size = (int(data[3][1]), int(data[3][1]))
 		outputs_size = (int(data[4][1]), int(data[4][1]))
-		show_capture = data[5][1]
+		show_capture = True if data[5][1] == "True" else False
 		time_cap = int(sys.argv[1]) if len(sys.argv) == 2 else int(data[6][1])
 
 		main(capture, path_model, path_label, inputs_size, outputs_size, show_capture, time_cap)
-
